96a512c42f50da2a1e8afe18766ff98a8fb240b5
HibaMCA2026 <hiba08052005@gmail.com>
2026-09-22 15:19:22 +0530

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,8 @@
+basic=float(input("Enter basic pay:"))
+HRA=10/100*basic
+TA=5/100*basic
+salary=basic+HRA+TA
+print("basic pay=",basic)
+print("HRA=",HRA)
+print("TA=",TA)
+print("Total salary=",salary)
